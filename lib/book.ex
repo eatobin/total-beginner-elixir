e15bb5cde316e03d32129907cbd252b2a348161c
Eric Tobin <eatobin@gmail.com>
2017-03-28 14:37:59 -0500

    great progess!

--- lib/book.ex
+++ lib/book.ex
@@ -1,17 +1,30 @@
 defmodule Book do
+
   defstruct [:title, :author, :borrower]
+
   def make_book(t, a), do: %Book{title: t, author: a, borrower: nil}
+
   def get_title(bk), do: bk.title
+
   def get_author(bk), do: bk.author
+
   def get_borrower(bk), do: bk.borrower
+
   def set_borrower(br, bk), do: %{bk | borrower: br}
+
   def available_string(bk) do
-  
+    br = get_borrower(bk)
+    case br do
+      nil -> "Available"
+        _ -> "Checked out to " <>
+               Borrower.get_name(br)
+    end
   end
-  def borrower_to_string(br) do
-    get_name(br) <>
-    " (" <>
-    to_string(get_max_books(br)) <>
-    " books)"
+
+  def book_to_string(bk) do
+    get_title(bk) <>
+      " by " <> get_author(bk) <>
+      "; " <> available_string(bk)
   end
+
 end
