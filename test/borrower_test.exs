461c41f74e69c2ab557db2d381668e0801e280d7
eatobin <eatobin@gmail.com>
2017-03-28 16:08:56 -0500

    make_borrower

--- test/borrower_test.exs
+++ test/borrower_test.exs
@@ -1,7 +1,7 @@
 defmodule BorrowerTest do
   use ExUnit.Case
 
-  @br %Borrower{name: "Borrower1", max_books: 1}
+  @br Borrower.make_borrower("Borrower1", 1)
 
   test "properly creates itself" do
     assert Borrower.get_name(@br) == "Borrower1"
