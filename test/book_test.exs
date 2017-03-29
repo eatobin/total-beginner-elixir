57373e47763ca1f581a0edac478827a731c21cfe
Eric Tobin <eatobin@gmail.com>
2017-03-28 19:28:07 -0500

    add book test

---
+++ test/book_test.exs
@@ -0,0 +1,10 @@
+defmodule BorrowerTest do
+  use ExUnit.Case
+
+  @br Borrower.make_borrower("Borrower1", 1)
+
+  test "properly creates itself" do
+    assert Borrower.get_name(@br) == "Borrower1"
+    assert Borrower.get_max_books(@br) == 1
+  end
+end
