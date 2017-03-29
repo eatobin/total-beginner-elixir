bd1998e74f524b6a3afb8d1f352c6ee6e176b97c
Eric Tobin <eatobin@gmail.com>
2017-03-29 14:02:40 -0500

    comment out

--- test/book_test.exs
+++ test/book_test.exs
@@ -1,10 +1,10 @@
-defmodule BorrowerTest do
-  use ExUnit.Case
+# defmodule BorrowerTest do
+#   use ExUnit.Case
 
-  @br Borrower.make_borrower("Borrower1", 1)
+#   @br Borrower.make_borrower("Borrower1", 1)
 
-  test "properly creates itself" do
-    assert Borrower.get_name(@br) == "Borrower1"
-    assert Borrower.get_max_books(@br) == 1
-  end
-end
+#   test "properly creates itself" do
+#     assert Borrower.get_name(@br) == "Borrower1"
+#     assert Borrower.get_max_books(@br) == 1
+#   end
+# end
