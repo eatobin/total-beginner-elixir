96bd98ff54196a50527f82d1f6aa53a1676784de
Eric Tobin <eatobin@gmail.com>
2017-03-29 21:09:41 -0500

    add set tests

--- test/borrower_test.exs
+++ test/borrower_test.exs
@@ -3,8 +3,13 @@ defmodule BorrowerTest do
 
   @br Borrower.make_borrower("Borrower1", 1)
 
-  test "properly creates itself" do
+  test "create itself properly" do
     assert Borrower.get_name(@br) == "Borrower1"
     assert Borrower.get_max_books(@br) == 1
+  end
+
+  test "set a new name and maxBooks" do
+    assert Borrower.set_name("Borrower1", %Borrower{name: "Jack", max_books: 1}) == @br
+    assert Borrower.set_max_books(1, %Borrower{name: "Borrower1", max_books: 11}) == @br
   end
 end
