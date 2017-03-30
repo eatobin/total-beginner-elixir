b40b0e7f766f26a1967d1a1d608cefc2309aa4b8
Eric Tobin <eatobin@gmail.com>
2017-03-30 16:11:49 -0500

    library progress

--- lib/library.ex
+++ lib/library.ex
@@ -3,7 +3,15 @@ defmodule Library do
     if Enum.member?(xs,x) do
       xs
     else
-    [x | xs]
+      [x | xs]
+    end
+  end
+
+  def remove_book(bk, bks) do
+    if Enum.member?(bks, bk) do
+      Enum.filter(bks, &(&1 != bk))
+    else
+      bks
     end
   end
 end
