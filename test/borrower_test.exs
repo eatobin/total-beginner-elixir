871c983bb461b26ac99efd5928438ea8116026b6
Eric Tobin <eatobin@gmail.com>
2017-03-29 22:51:53 -0500

    borrower tests finished

--- test/borrower_test.exs
+++ test/borrower_test.exs
@@ -12,4 +12,8 @@ defmodule BorrowerTest do
     assert Borrower.set_name("Borrower1", %Borrower{name: "Jack", max_books: 1}) == @br
     assert Borrower.set_max_books(1, %Borrower{name: "Borrower1", max_books: 11}) == @br
   end
+
+  test "return a string \"Borrower1 (1 books)\"" do
+    assert Borrower.borrower_to_string(@br) == "Borrower1 (1 books)"
+  end
 end
