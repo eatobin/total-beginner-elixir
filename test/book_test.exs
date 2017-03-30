4f445228ff49dc6e2e1891788c155acd3822319c
Eric Tobin <eatobin@gmail.com>
2017-03-29 23:24:36 -0500

    book test progress!

--- test/book_test.exs
+++ test/book_test.exs
@@ -1,10 +1,12 @@
-# defmodule BorrowerTest do
-#   use ExUnit.Case
+defmodule BookTest do
+  use ExUnit.Case
 
-#   @br Borrower.make_borrower("Borrower1", 1)
+  @br Borrower.make_borrower("Borrower1", 1)
+  @bk1 Book.make_book("Title1", "Author1")
+  @bk2 Book.set_borrower(@br, @bk1)
 
-#   test "properly creates itself" do
-#     assert Borrower.get_name(@br) == "Borrower1"
-#     assert Borrower.get_max_books(@br) == 1
-#   end
-# end
+  test "create itself properly" do
+    assert Book.get_title(@bk1) == "Title1"
+    assert Book.get_author(@bk1) == "Author1"
+  end
+end
