162f74ccd0d1153ad2be3125b0408a41510950df
Eric Tobin <eatobin@gmail.com>
2017-04-02 17:16:12 -0500

    first library tests work!

--- test/library_test.exs
+++ test/library_test.exs
@@ -14,14 +14,21 @@ defmodule LibraryTest do
   @bk1 Book.make_book("Title1", "Author1", @br1)
   @bk2 Book.make_book("Title2", "Author2", nil)
   @bk3 Book.make_book("Title3", "Author3", @br3)
-  @bk4 Book.make_book("Title4", "Author4", Borrower.make_borrower("Borrower3", 3))
+  @bk4 Book.make_book("Title4", "Author4", %Borrower{max_books: 3, name: "Borrower3"})
 
-  #test "create itself properly" do
-  #  assert Book.get_title(@bk1) == "Title1"
-  #  assert Book.get_author(@bk1) == "Author1"
-  #  assert Book.get_borrower(@bk1) == nil
-  #  assert Book.get_borrower(@bk2) == @br2
-  #end
+  @bks1 [@bk1, @bk2]
+  @bks2 [@bk3, @bk1, @bk2]
+  @bks3 [@bk1, @bk2, @bk3, @bk4]
+
+  @ss "\n--- Status Report of Test Library ---\n\nTest Library: 3 books; 3 borrowers.\n\nTitle3 by Author3; Checked out to Borrower3\nTitle1 by Author1; Checked out to Borrower1\nTitle2 by Author2; Available\n\nBorrower3 (3 books)\nBorrower1 (1 books)\nBorrower2 (2 books)\n\n--- End of Status Report ---\n"
+
+  test "add a Borrower correctly" do
+    assert Library.add_item(@br3, @brs1) == @brs2
+    assert Library.add_item(@br2, @brs1) == @brs1
+
+    assert Library.add_item(@bk3, @bks1) == @bks2
+    assert Library.add_item(@bk2, @bks1) == @bks1
+  end
 
   #test "return a string \"Title1 by Author1; Available\"" do
   #  assert Book.book_to_string(@bk1) == "Title1 by Author1; Available"
