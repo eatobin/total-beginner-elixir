6d3e7788790119c89e4c4aaba431c084f2d4a397
Eric Tobin <eatobin@gmail.com>
2017-04-01 22:18:55 -0500

    do and ends on all

--- lib/borrower.ex
+++ lib/borrower.ex
@@ -1,15 +1,25 @@
 defmodule Borrower do
   defstruct [:name, :max_books]
 
-  def make_borrower(n, mb), do: %Borrower{name: n, max_books: mb}
+  def make_borrower(n, mb) do
+    %Borrower{name: n, max_books: mb}
+  end
 
-  def get_name(br), do: br.name
+  def get_name(br) do
+    br.name
+  end
 
-  def set_name(n, br), do: %{br | name: n}
+  def set_name(n, br) do
+    %{br | name: n}
+  end
 
-  def get_max_books(br), do: br.max_books
+  def get_max_books(br) do
+    br.max_books
+  end
 
-  def set_max_books(mb, br), do: %{br | max_books: mb}
+  def set_max_books(mb, br) do
+    %{br | max_books: mb}
+  end
 
   def borrower_to_string(br) do
     get_name(br) <>
