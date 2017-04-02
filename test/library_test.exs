96efa2c72980e77e005e17261a4503df953b0bfe
Eric Tobin <eatobin@gmail.com>
2017-04-02 16:19:01 -0500

    library test progress

--- test/library_test.exs
+++ test/library_test.exs
@@ -11,22 +11,24 @@ defmodule LibraryTest do
   @brs1 [@br1, @br2]
   @brs2 [@br3, @br1, @br2]
 
-  @bk1 Book.make_book("Title1", "Author1", nil)
-  @bk2 Book.set_borrower(@br2, @bk1)
+  @bk1 Book.make_book("Title1", "Author1", @br1)
+  @bk2 Book.make_book("Title2", "Author2", nil)
+  @bk3 Book.make_book("Title3", "Author3", @br3)
+  @bk4 Book.make_book("Title4", "Author4", Borrower.make_borrower("Borrower3", 3))
 
-  test "create itself properly" do
-    assert Book.get_title(@bk1) == "Title1"
-    assert Book.get_author(@bk1) == "Author1"
-    assert Book.get_borrower(@bk1) == nil
-    assert Book.get_borrower(@bk2) == @br2
-  end
+  #test "create itself properly" do
+  #  assert Book.get_title(@bk1) == "Title1"
+  #  assert Book.get_author(@bk1) == "Author1"
+  #  assert Book.get_borrower(@bk1) == nil
+  #  assert Book.get_borrower(@bk2) == @br2
+  #end
 
-  test "return a string \"Title1 by Author1; Available\"" do
-    assert Book.book_to_string(@bk1) == "Title1 by Author1; Available"
-  end
+  #test "return a string \"Title1 by Author1; Available\"" do
+  #  assert Book.book_to_string(@bk1) == "Title1 by Author1; Available"
+  #end
 
-  test "return a string \"Title1 by Author1; Checked out to Borrower2\"" do
-    assert Book.book_to_string(@bk2) == "Title1 by Author1; Checked out to Borrower2"
-  end
+  #test "return a string \"Title1 by Author1; Checked out to Borrower2\"" do
+  #  assert Book.book_to_string(@bk2) == "Title1 by Author1; Checked out to Borrower2"
+  #end
 
 end
