4c9e5e70acf2c6ff74c17b3d7fb0188b98ae6ab0
Eric Tobin <eatobin@gmail.com>
2023-08-05 20:07:47 -0500

    bump elixir to 15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TotalBeginnerElixir.Mixfile do
     [
       app: :total_beginner_elixir,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
